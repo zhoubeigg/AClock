5c1dd4c7987d3239f28c22219f13bf93450faad6
zhouebisg <zhoubeisg@163.com>
2015-09-09 14:20:42 +0800

    初始化项目

---
+++ AClock/AClock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AClock
+//
+//  Created by macAir on 15/9/9.
+//  Copyright (c) 2015年 ZhouBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AClock/AClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AClock
+//
+//  Created by macAir on 15/9/9.
+//  Copyright (c) 2015年 ZhouBei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
